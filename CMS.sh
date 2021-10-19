bedc7800817342aafd784cd895d1b79464e03f0a
KamilKrauze <kamila.p.krauze@gmail.com>
2021-10-19 20:28:28 +0100

    IF to CASE
    Converted the conditions from the first  IF to CASE.

--- CMS.sh
+++ CMS.sh
@@ -18,58 +18,56 @@ do
 	read -p ">" userIn
 	inputArr=($userIn) #Separate strings if there is a space between them and pass into array - https://stackoverflow.com/questions/1469849/how-to-split-one-string-into-multiple-strings-separated-by-at-least-one-space-in - Date Visited: 19.10.2021
 
-	if [ "${inputArr[0]}" == "help" ]
-	then
+	case ${inputArr[0]} in
+		"help")
 		if [ ${#inputArr[@]} -eq 2 ]; #Checks if the array has 2 elements
 		then
-			if [[ ${inputArr[@]:1} =~ $re ]]; #Check if variable is an integer - https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash/806923 - Date Visited: 19.10.2021
+			if ! [[ ${inputArr[@]:1} =~ $re ]]; #Check if variable is an integer - https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash/806923 - Date Visited: 19.10.2021
 			then
 				echo "Help me!" #Run help script
-				echo "${inputArr[@]:1}"
+				./help.sh "${inputArr[@]:1}"
 			else
-				echo -e "Incorrect argument. Argument is an integer.\n\t-E.g - ${BLUE}help${NC} ${CYAN}3${NC}"
+				echo -e "Incorrect argument. Argument should be an integer.\n\t-E.g - ${BLUE}help${NC} ${CYAN}3${NC}"
 			fi
 		else
 			echo -e "Not enough or too many arguments for ${BLUE}'help'${NC}\n\t-Try ${BLUE}help${NC} ${CYAN}<pageNo>${NC}\n\t-E.g - ${BLUE}help${NC} ${CYAN}3${NC}"
 		fi
-	
-	elif [ "${inputArr[0]}" == "mkrepo" ];
-	then
-		echo "Create new repo" #Runs Create repo script 
-	
-	elif [ "${inputArr[0]}" == "openrepo" ];
-	then
-		echo "Opens repo" #Runs open repo script
-	
-	elif [ "${inputArr[0]}" == "addfiles" ];
-	then
-		echo "Files have been added" #Add file script
-		#"${inputArr[@]:1}" #Get every element within the array except the first - https://stackoverflow.com/questions/6287419/getting-all-elements-of-a-bash-array-except-the-first - Date Visited: 19.10.2021
+		;;
+		"mkrepo")
+			echo "Create new repo" #Runs Create repo script
+			;;
+		"openrepo")
+			echo "Opens repo" #Runs open repo script
+			;;
+		"addfiles")
+			echo "Files have been added" #Add file script
+			#"${inputArr[@]:1}" #Get every element within the array except the first - https://stackoverflow.com/questions/6287419/getting-all-elements-of-a-bash-array-except-the-first - Date Visited: 19.10.2021
+			;;
+		"checkin")
+			canCheckin=false
+			export canCheckin
+			export canCheckout
 
-	elif [ "${inputArr[0]}" == "checkin" ];
-	then
-		canCheckin=false
-		export canCheckin
-		export canCheckout
+			echo "Pushing to repo..." #Runs check in script
+			./checkin.sh
+			;;
 
-		echo "Pushing to repo..." #Runs check in script
-		./checkin.sh
+		"checkout")
+			canCheckout=false
+			export canCheckin
+			export canCheckout
 
-	elif [ "${inputArr[0]}" == "checkout" ];
-	then
-		canCheckout=false
-		export canCheckin
-		export canCheckout
-
-		echo "Pulling from repo..." #Runs check out script
-		./checkout.sh
+			echo "Pulling from repo..." #Runs check out script
+			./checkout.sh
+			;;
 	
-	elif [ "${inputArr[0]}" == "quit" ];
-	then
-		echo -e "Quiting..." #Quits the script
+		"quit")
+			echo -e "Quiting..." #Quits the script
+			;;
 	
-	else
+		*)
 		echo -e "\t${RED}Command '$userIn' does not exist...${NC}\n\t\v- Use the ${BLUE}'help'${NC} command to get a list of commands this script supports." #Outputs if user input in incorrect
 		continue
-	fi
+		;;
+	esac
 done
