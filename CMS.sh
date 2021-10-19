01f64d434ab21bbe440d0f3fc002c560b427eae5
KamilKrauze <kamila.p.krauze@gmail.com>
2021-10-19 17:35:32 +0100

    Added the possibility of arguments to CMS.sh
    You can have arguments in-front of commands.
    
    e.g: addfiles <filename> <filename> ...

--- CMS.sh
+++ CMS.sh
@@ -3,37 +3,54 @@ RED="\033[0;31m"
 BLUE="\033[0;34m"
 NC="\033[0m"
 
+canCheckin=true
+canCheckout=true
+
 userIn="."
-while [ "$userIn" != "quit" ]
+inputArr=($userIn)
+
+while [ "${inputArr[0]}" != "quit" ]
 do
 	read -p "> " userIn
+	inputArr=($userIn) #Separate strings if there is a space between them and pass into array - https://stackoverflow.com/questions/1469849/how-to-split-one-string-into-multiple-strings-separated-by-at-least-one-space-in - Date Visited: 19.10.2021
 
-	if [ "$userIn" == "help" ]
+	if [ "$1" == "help" ]
 	then
 		echo "Help me!" #Run help script
 		../Bash\ Scripts/functionTest.sh
 	
-	elif [ "$userIn" == "mkrepo" ];
+	elif [ "${inputArr[0]}" == "mkrepo" ];
 	then
 		echo "Create new repo" #Runs Create repo script 
 	
-	elif [ "$userIn" == "openrepo" ];
+	elif [ "${inputArr[0]}" == "openrepo" ];
 	then
 		echo "Opens repo" #Runs open repo script
 	
-	elif [ "$userIn" == "addfiles" ];
+	elif [ "${inputArr[0]}" == "addfiles" ];
 	then
 		echo "Files have been added" #Add file script
+		#"${inputArr[@]:1}" #Get every element within the array except the first - https://stackoverflow.com/questions/6287419/getting-all-elements-of-a-bash-array-except-the-first - Date Visited: 19.10.2021
 
-	elif [ "$userIn" == "checkin" ];
+	elif [ "${inputArr[0]}" == "checkin" ];
 	then
+		canCheckin=false
+		export canCheckin
+		export canCheckout
+
 		echo "Pushing to repo..." #Runs check in script
+		./checkin.sh
 
-	elif [ "$userIn" == "checkout" ];
+	elif [ "${inputArr[0]}" == "checkout" ];
 	then
+		canCheckout=false
+		export canCheckin
+		export canCheckout
+
 		echo "Pulling from repo..." #Runs check out script
+		./checkout.sh
 	
-	elif [ "$userIn" == "quit" ];
+	elif [ "${inputArr[0]}" == "quit" ];
 	then
 		echo -e "Quiting..." #Quits the script
 		sleep .7
