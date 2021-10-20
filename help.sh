9554016e3fa84c7b4015ec916914b5c7e1a43f99
KamilKrauze <kamila.p.krauze@gmail.com>
2021-10-20 14:13:17 +0100

    Added 2 commands to the command list

--- help.sh
+++ help.sh
@@ -10,17 +10,20 @@ function page(){
 	echo -e "\t#################################"
 }
 
+function commandList()
+{
+	echo -e "- ${BLUE}makerepo${NC} ${CYAN}<repoName>${NC} ${CYAN}<repoLocation>${NC} ${CYAN}<workingDirectory>${NC} - Creates a repository with a user specified name, the repository's location and working directory is specified by the user."
+	echo -e "\n- ${BLUE}openrepo${NC} ${CYAN}<repoName>${NC} - Opens a repository, by specfying the repository's name."
+}
+
 arr=($@)
 
-# for i in "${arr[@]:i}"
-# 	do
-# 		echo $i
-# 	done
 if [ ${#arr[@]} -eq 1 ]; #Checks if the array has 2 elements
 then
 	if ! [[ ${arr[@]} =~ $re ]]; #Check if variable is an integer - https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash/806923 - Date Visited: 19.10.2021
 	then
-		page ${arr[@]}	
+		page ${arr[@]}
+		commandList
 	else
 		echo -e "Incorrect argument. Argument should be an integer.\n\t-E.g - ${BLUE}help${NC} ${CYAN}3${NC}"
 	fi
