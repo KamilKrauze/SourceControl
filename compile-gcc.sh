b1c21261ded6a970277caaf967c7a4152c7e9254
KamilKrauze <kamila.p.krauze@gmail.com>
2021-10-31 13:11:19 +0000

    GCC compiler fix

--- compile-gcc.sh
+++ compile-gcc.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+#regex='gcc+$'
+
+gcc --version >/dev/null
+if ! [ $? -eq 0 ]; then
+	echo -e "GCC compiler not installed on this system. Please install to use this feature."
+	exit
+fi
+
 repositoryPath=$(grep -w $1 repository-index.txt | cut -d ';' -f2)
 
-coutput=($(find $repositoryPath -type f -name "*.c")) # Store output from the find command into variable - https://stackoverflow.com/questions/2087001/how-can-i-process-the-results-of-find-in-a-bash-script/2087038 - 30/10/2021
+# "-path $repositoryPath/.vc -prune" tells the find command to ignore a specific directory - https://linuxconfig.org/how-to-explicitly-exclude-directory-from-find-command-s-search- 30/10/21 
 
-houtput=($(find $repositoryPath -type f -name "*.h")) # Store output from the find command into variable - https://stackoverflow.com/questions/2087001/how-can-i-process-the-results-of-find-in-a-bash-script/2087038 - 30/10/2021
+coutput=($(find $repositoryPath -type f \( ! -wholename $repositoryPath/.vc \) -name "*.c")) # Store output from the find command into variable - https://stackoverflow.com/questions/2087001/how-can-i-process-the-results-of-find-in-a-bash-script/2087038 - 30/10/2021
+houtput=($(find $repositoryPath -type f \( ! -wholename $repositoryPath/.vc \) -name "*.h")) # Store output from the find command into variable - https://stackoverflow.com/questions/2087001/how-can-i-process-the-results-of-find-in-a-bash-script/2087038 - 30/10/2021
+
+if [ -z ${coutput[0]} ]; then
+	echo "No source files found... exiting"
+	exit
+fi
 
 read -p "Executable name: " execName
 
