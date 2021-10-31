28e79049ffbe53fd90de0e1a07ee79c758b28b2d
KamilKrauze <kamila.p.krauze@gmail.com>
2021-10-31 13:42:46 +0000

    Updated comment

--- compile-javac.sh
+++ compile-javac.sh
@@ -8,7 +8,7 @@ fi
 
 repositoryPath=$(grep -w $1 repository-index.txt | cut -d ';' -f2)
 
-# "-path $repositoryPath/.vc -prune" tells the find command to ignore a specific directory - https://linuxconfig.org/how-to-explicitly-exclude-directory-from-find-command-s-search- 30/10/21 
+# "\( ! -wholename $repositoryPath/.vc \)" tells the find command to ignore a specific directory - https://linuxconfig.org/how-to-explicitly-exclude-directory-from-find-command-s-search- 30/10/21 
 
 coutput=($(find $repositoryPath -type f \( ! -wholename $repositoryPath/.vc \) -name "*.java")) # Store output from the find command into variable - https://stackoverflow.com/questions/2087001/how-can-i-process-the-results-of-find-in-a-bash-script/2087038 - 30/10/2021
 
