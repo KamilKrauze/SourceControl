e7372df64d0f4f9c838dffe8f864f6e8f6847420
KamilKrauze <kamila.p.krauze@gmail.com>
2021-10-31 17:43:32 +0000

    Added comment

--- list-repos.sh
+++ list-repos.sh
@@ -6,8 +6,10 @@ env | grep -q NC
 
 echo -e "Here are your currently existing repositories: "
 
+# Read line by line from file and split line by delimeter - https://zaiste.net/posts/shell-split-string-reading-file/ - 31/10/2021
 while IFS=';' read -r repoName repoPath
 do
 	echo -e "${BLUE}$repoName${NC}"
 done < repository-index.txt
+
 echo -e "${CYAN}----------<<<<< End of File >>>>>----------${NC}\n\n"
